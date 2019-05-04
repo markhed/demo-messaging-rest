6d69abe337a10b9a1043cb03267a709b7cc43cde
Mark Harun <markharun@gmail.com>
2019-05-04 09:51:36 +0200

    Initialized project

---
+++ src/main/java/co/markhed/demo/messaging/DemoRestMessagingApplication.java
@@ -0,0 +1,13 @@
+package co.markhed.demo.messaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRestMessagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRestMessagingApplication.class, args);
+	}
+
+}
